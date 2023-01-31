fd043cad0c5e8dfbd39fffcc6de91328fb940c51
m1212e <->
2023-01-31 02:41:39 +0100

    bbvn

--- src/routes/association/bbvn/+page.svelte
+++ src/routes/association/bbvn/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import type { BBVN } from 'src/interfaces/directus';
 	import { getTranslation } from 'src/services/language';
-	import DarkMail from 'src/icons/dark-mail.svg';
+	import Mail from 'src/icons/dark-mail.svg';
+	import UNLogo from 'src/icons/dark-un-logo.svg';
+	import ShieldIcon from 'src/icons/dark-shield-icon.svg';
+	import Rights from 'src/icons/dark-rights.svg';
+	import Sustainable from 'src/icons/dark-sustainable.svg';
 
 	export let data: any;
 	let content = data.content as BBVN;
 	let translated = getTranslation(content);
+
+	let selected = 0;
 </script>
 
 <div class="sm:flex">
@@ -17,10 +23,41 @@
 			{@html $translated.book_now}
 		</p>
 		<div class="flex mt-5">
-			<img src={DarkMail} alt="mail icon" class="mr-2 w-14" />
+			<img src={Mail} alt="mail icon" class="mr-2 w-14" />
 			<p>
 				{@html $translated.contact}
 			</p>
 		</div>
 	</div>
 </div>
+
+<div class="flex mt-20 justify-between">
+	<img
+		on:click={() => (selected = 0)}
+		src={UNLogo}
+		alt="un logo"
+		class="mr-2 w-1/4 px-5 hover:scale-110 duration-300 cursor-pointer"
+	/>
+	<img
+		on:click={() => (selected = 0)}
+		src={ShieldIcon}
+		alt="un logo"
+		class="mr-2 w-1/4 px-5 scale-75 hover:scale-90 duration-300 cursor-pointer"
+	/>
+	<img
+		on:click={() => (selected = 0)}
+		src={Rights}
+		alt="un logo"
+		class="mr-2 w-1/4 px-5 hover:scale-110 duration-300 cursor-pointer"
+	/>
+	<img
+		on:click={() => (selected = 0)}
+		src={Sustainable}
+		alt="un logo"
+		class="mr-2 w-1/4 px-5 hover:scale-110 duration-300 cursor-pointer"
+	/>
+</div>
+
+{#if selected == 0}{:else if selected == 1}{:else if selected == 2}{:else if selected == 3}{:else}
+	Error
+{/if}
