cfe7f3c9f1c7822511a9ac219880d341788c2680
m1212e <->
2023-04-06 19:28:58 +0200

    Implemented debouncer for build webhook

--- build.js
+++ build.js
@@ -1,15 +1,27 @@
 import * as http from "http";
 import {build} from 'vite'
 
-const requestListener = function(req, res) {
-    try {
+let debounceTimeout;
+
+function run() {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+        console.log(`[${(new Date()).toISOString()}] Running build...`);
         ; (async () => {
             await build({
                 root: ".",
             })
+            console.log(`[${(new Date()).toISOString()}] Done!`);
         })()
+    }, 5000);
+}
+
+const requestListener = function(req, res) {
+    console.log(`[${(new Date()).toISOString()}] Recieved request, waiting for debounce...`);
+    try {
+        run();
     } catch (error) {
-        console.log(`[${Date.now()}] Error while building server: ${error}`);
+        console.log(`[${(new Date()).toISOString()}] Error while building server: ${error}`);
         res.writeHead(400);
         res.end("Internal server error");
         return;
@@ -22,7 +34,7 @@ const host = process.env.HOST ?? "0.0.0.0";
 const port = process.env.PORT ?? "8000";
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`[${Date.now()}] Build server listening on http://${host}:${port}`);
+    console.log(`[${(new Date()).toISOString()}] Build server listening on http://${host}:${port}`);
 });
 
 
