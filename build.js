7d5306b148787f5557e5b7e3b767e4afd9f42955
m1212e <->
2023-04-06 19:18:03 +0200

    Added timestamp to console logs

--- build.js
+++ build.js
@@ -2,11 +2,18 @@ import * as http from "http";
 import {build} from 'vite'
 
 const requestListener = function(req, res) {
-    ; (async () => {
-        await build({
-            root: ".",
-        })
-    })()
+    try {
+        ; (async () => {
+            await build({
+                root: ".",
+            })
+        })()
+    } catch (error) {
+        console.log(`[${Date.now()}] Error while building server: ${error}`);
+        res.writeHead(400);
+        res.end("Internal server error");
+        return;
+    }
     res.writeHead(200);
     res.end("OK");
 };
@@ -15,7 +22,7 @@ const host = process.env.HOST ?? "0.0.0.0";
 const port = process.env.PORT ?? "8000";
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Build server listening on http://${host}:${port}`);
+    console.log(`[${Date.now()}] Build server listening on http://${host}:${port}`);
 });
 
 
